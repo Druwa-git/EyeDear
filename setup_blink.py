45f8f5ea67904e4ae3935950b74977c1ae6e1290
lltera <dlrmslthem@naver.com>
2021-07-11 21:17:01 +0900

    custom_blink_train

--- setup_blink.py
+++ setup_blink.py
@@ -29,17 +29,10 @@ while webcam.isOpened():
     cv2.putText(frame, text_push, (20, 130), cv2.FONT_HERSHEY_DUPLEX, 0.8, (255, 255, 255), 1)
     cv2.imshow("SetUp", frame)
     
-    #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    #faces = gaze._face_detector(gray)
-    #for face in faces:
-    #    shapes = gaze._predictor(gray, face)
-    #    shapes = face_utils.shape_to_np(shapes)
-    #    eye_img_l, eye_rect_l = gaze.eye_left.crop_eye(gray, eye_points=shapes[36:42])
-    #cv2.rectangle(frame, pt1=tuple(eye_rect_l[0:2]), pt2=tuple(eye_rect_l[2:4]), color=(255,255,255), thickness=2)
     faces = gaze._face_detector(frame)
     #save open img
     if cv2.waitKey(1) == ord('v') and direction_index == 0: 
-        if setCount > 10:
+        if setCount > 20:
             direction_index += 1
             setCount = 0
         if gaze.pupils_located:
@@ -48,15 +41,17 @@ while webcam.isOpened():
             eye_img_l, eye_rect_l = gaze.eye_left.crop_eye(frame, eye_points=landmarks[36:42])
             eye_img_r, eye_rect_r = gaze.eye_left.crop_eye(frame, eye_points=landmarks[42:48])
             eye_img_l = cv2.resize(eye_img_l, dsize=IMG_SIZE)
+            eye_img_l = cv2.cvtColor(eye_img_l, cv2.COLOR_BGR2GRAY)
             eye_img_r = cv2.resize(eye_img_r, dsize=IMG_SIZE)
-            cv2.imwrite('./img/open Left %d .jpg' %setCount ,eye_img_l, params=[cv2.IMWRITE_JPEG_QUALITY,100])
-            cv2.imwrite('./img/open Right %d .jpg' %setCount ,eye_img_r, params=[cv2.IMWRITE_JPEG_QUALITY,100])
+            eye_img_r = cv2.cvtColor(eye_img_r, cv2.COLOR_BGR2GRAY)
+            cv2.imwrite('./dataset/open_Left%d.jpg' %setCount ,eye_img_l, params=[cv2.IMWRITE_JPEG_QUALITY,100])
+            cv2.imwrite('./dataset/open_Right%d.jpg' %setCount ,eye_img_r, params=[cv2.IMWRITE_JPEG_QUALITY,100])
             print(setCount, direction[direction_index], ' save img')
             setCount += 1
     #save close img
     elif cv2.waitKey(1) == ord('b') and direction_index == 1:
         
-        if setCount > 10:
+        if setCount > 20:
             direction_index += 1
             setCount = 0
             
@@ -66,9 +61,11 @@ while webcam.isOpened():
             eye_img_l, eye_rect_l = gaze.eye_left.crop_eye(frame, eye_points=landmarks[36:42])
             eye_img_r, eye_rect_r = gaze.eye_left.crop_eye(frame, eye_points=landmarks[42:48])
             eye_img_l = cv2.resize(eye_img_l, dsize=IMG_SIZE)
+            eye_img_l = cv2.cvtColor(eye_img_l, cv2.COLOR_BGR2GRAY)
             eye_img_r = cv2.resize(eye_img_r, dsize=IMG_SIZE)
-            cv2.imwrite('./img/close Left %d .jpg' %setCount ,eye_img_l, params=[cv2.IMWRITE_JPEG_QUALITY,100])
-            cv2.imwrite('./img/close Right %d .jpg' %setCount ,eye_img_r, params=[cv2.IMWRITE_JPEG_QUALITY,100])
+            eye_img_r = cv2.cvtColor(eye_img_r, cv2.COLOR_BGR2GRAY)
+            cv2.imwrite('./dataset/close_Left%d.jpg' %setCount ,eye_img_l, params=[cv2.IMWRITE_JPEG_QUALITY,100])
+            cv2.imwrite('./dataset/close_Right%d.jpg' %setCount ,eye_img_r, params=[cv2.IMWRITE_JPEG_QUALITY,100])
             print(setCount, direction[direction_index], ' save img')
             setCount += 1
     
