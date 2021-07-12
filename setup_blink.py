fa4ee5964893c0971876ec032c08049ad899832d
lltera <82861314+lltera@users.noreply.github.com>
2021-07-12 14:53:28 +0900

    blinking train
    modify path & save only left eye

--- setup_blink.py
+++ setup_blink.py
@@ -39,13 +39,9 @@ while webcam.isOpened():
             landmarks = gaze._predictor(frame, faces[0])
             landmarks = face_utils.shape_to_np(landmarks)
             eye_img_l, eye_rect_l = gaze.eye_left.crop_eye(frame, eye_points=landmarks[36:42])
-            eye_img_r, eye_rect_r = gaze.eye_left.crop_eye(frame, eye_points=landmarks[42:48])
             eye_img_l = cv2.resize(eye_img_l, dsize=IMG_SIZE)
             eye_img_l = cv2.cvtColor(eye_img_l, cv2.COLOR_BGR2GRAY)
-            eye_img_r = cv2.resize(eye_img_r, dsize=IMG_SIZE)
-            eye_img_r = cv2.cvtColor(eye_img_r, cv2.COLOR_BGR2GRAY)
             cv2.imwrite('./dataset/open_Left%d.jpg' %setCount ,eye_img_l, params=[cv2.IMWRITE_JPEG_QUALITY,100])
-            cv2.imwrite('./dataset/open_Right%d.jpg' %setCount ,eye_img_r, params=[cv2.IMWRITE_JPEG_QUALITY,100])
             print(setCount, direction[direction_index], ' save img')
             setCount += 1
     #save close img
@@ -55,17 +51,13 @@ while webcam.isOpened():
             direction_index += 1
             setCount = 0
             
-        if not gaze.pupils_located:
+        if faces[0]:
             landmarks = gaze._predictor(frame, faces[0])
             landmarks = face_utils.shape_to_np(landmarks)
             eye_img_l, eye_rect_l = gaze.eye_left.crop_eye(frame, eye_points=landmarks[36:42])
-            eye_img_r, eye_rect_r = gaze.eye_left.crop_eye(frame, eye_points=landmarks[42:48])
             eye_img_l = cv2.resize(eye_img_l, dsize=IMG_SIZE)
             eye_img_l = cv2.cvtColor(eye_img_l, cv2.COLOR_BGR2GRAY)
-            eye_img_r = cv2.resize(eye_img_r, dsize=IMG_SIZE)
-            eye_img_r = cv2.cvtColor(eye_img_r, cv2.COLOR_BGR2GRAY)
             cv2.imwrite('./dataset/close_Left%d.jpg' %setCount ,eye_img_l, params=[cv2.IMWRITE_JPEG_QUALITY,100])
-            cv2.imwrite('./dataset/close_Right%d.jpg' %setCount ,eye_img_r, params=[cv2.IMWRITE_JPEG_QUALITY,100])
             print(setCount, direction[direction_index], ' save img')
             setCount += 1
     
